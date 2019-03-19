6227f2f0f9869df04678e8d12ced6f47fa3edd71
Jeewhan <distinct19@gmail.com>
2019-03-19 14:37:41 +0900

    Fail : pipe, go

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
 const _ = Symbol("parameter");
 const ___ = Symbol("rest parameters");
 
-const reduce = function() {};
+const reduce = function(f, acc, iter) {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
 
-const map = function() {};
+  for (const v of iter) {
+    acc = f(acc, v);
+  }
 
-const filter = function() {};
+  return acc;
+};
 
-const groupBy = function() {};
+const map = function(f, iter) {
+  const fn = (acc, v) => acc.concat(f(v));
+  return reduce(fn, [], iter);
+};
 
-const countBy = function() {};
+const filter = function(f, iter) {
+  const fn = (acc, v) => (f(v) ? acc.concat(v) : acc);
+  return reduce(fn, [], iter);
+};
 
-const indexBy = function() {};
+const groupBy = function(f, iter) {
+  const fn = (acc, v) => {
+    const k = f(v);
+    if (!acc[k]) acc[k] = [v];
+    else acc[k].push(v);
+    return acc;
+  };
+  return reduce(fn, {}, iter);
+};
 
-const pipe = function() {};
+const countBy = function(f, iter) {
+  const fn = (acc, v) => {
+    const k = f(v);
+    if (!acc[k]) acc[k] = 1;
+    else acc[k] += 1;
+    return acc;
+  };
+  return reduce(fn, {}, iter);
+};
 
-const go = function() {};
+const indexBy = function(f, iter) {
+  const fn = (acc, v) => {
+    const k = f(v);
+    const key = typeof k !== "string" ? String(k) : k;
+    acc[k] = v;
+    return acc;
+  };
+  return reduce(fn, {}, iter);
+};
+
+function isIterable(target) {
+  return target != null && Symbol.iterator in Object(target);
+}
+
+const pipe = function(...fs) {
+  const fn = (acc, f) => (isIterable(acc) ? f(...acc) : f(acc));
+  return (...iter) => reduce(fn, iter, fs);
+};
+
+const go = function(iter, ...fs) {
+  return pipe(...fs)(...iter);
+};
 
 const curry = function() {};
 
@@ -34,4 +84,4 @@ export {
   partial,
   _,
   ___,
-}
+};
